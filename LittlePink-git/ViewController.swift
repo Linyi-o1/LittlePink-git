a5baa5bd292f1cdead9fd053c90b6f610d3be4c4
白旭君 <xujun.bai@changhong.com>
2023-09-14 13:51:50 +0800

    Initial Commit

---
+++ LittlePink-git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink-git
+//
+//  Created by 临易 on 2023/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
